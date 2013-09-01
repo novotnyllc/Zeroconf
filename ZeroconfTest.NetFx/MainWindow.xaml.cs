8fc46969ff99a39362d7e5778e9234aea7bea812
Oren Novotny <oren@novotny.org>
2013-09-01 16:11:18 -0400

    Add .Net desktop verion

---
+++ ZeroconfTest.NetFx/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zeroconf;
+
+namespace ZeroconfTest.NetFx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IDisposable _d;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_d != null)
+                _d.Dispose();
+            
+            _d = ZeroconfResolver
+                .Resolve("_p2pchat._udp.local.")
+                .Timeout(TimeSpan.FromSeconds(5))
+                .Subscribe(x => Debug.WriteLine(x));
+        }
+    }
+}
