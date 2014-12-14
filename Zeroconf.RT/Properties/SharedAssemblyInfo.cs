10836f88729ce0359967739999c155f2b66e8074
Oren Novotny <oren@novotny.org>
2014-12-13 19:40:12 -0500

    Update nuspec for 2.3

--- Zeroconf.RT/Properties/SharedAssemblyInfo.cs
+++ Zeroconf.RT/Properties/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("Zeroconf")]
-[assembly: AssemblyDescription("Bonjour support for .NET 4.5, Windows Phone 8 and Windows Store apps")]
+[assembly: AssemblyDescription("Bonjour support for .NET 4.5, Windows Phone 8, Windows Store apps and Xamarin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Zeroconf.RT")]
-[assembly: AssemblyCopyright("Copyright © Oren Novotny & Sergey Aldoukhov 2013")]
+[assembly: AssemblyProduct("Zeroconf")]
+[assembly: AssemblyCopyright("Copyright © Oren Novotny 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
