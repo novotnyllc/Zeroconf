7485c48008fb73e3c894da0b7d7d89df0e08ddb3
Oren Novotny <oren@novotny.org>
2016-04-29 08:20:16 -0400

    remove old attributes

--- Zeroconf/Properties/AssemblyInfo.cs
+++ Zeroconf/Properties/AssemblyInfo.cs
@@ -6,6 +6,3 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-
-[assembly: InternalsVisibleTo("Zeroconf.Platform")]
