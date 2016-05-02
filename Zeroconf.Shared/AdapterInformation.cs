fd1756f1ad299ec0485bc9f31944c8fdd53f0d44
Oren Novotny <oren@novotny.org>
2016-05-01 21:55:35 -0400

    Convert to struct

--- Zeroconf.Shared/AdapterInformation.cs
+++ Zeroconf.Shared/AdapterInformation.cs
@@ -1,29 +1,54 @@
-﻿namespace Zeroconf
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zeroconf
 {
-    public class AdapterInformation
+    public struct AdapterInformation : IEquatable<AdapterInformation>
     {
-        private readonly string m_address;
-        private readonly string m_name;
+        public bool Equals(AdapterInformation other)
+        {
+            return string.Equals(Address, other.Address) && string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is AdapterInformation && Equals((AdapterInformation)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Address != null ? Address.GetHashCode() : 0)*397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+
+        public static bool operator ==(AdapterInformation left, AdapterInformation right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(AdapterInformation left, AdapterInformation right)
+        {
+            return !left.Equals(right);
+        }
 
         public AdapterInformation(string address, string name)
         {
-            m_address = address;
-            m_name = name;
+            Address = address;
+            Name = name;
         }
 
-        public string Address
-        {
-            get { return m_address; }
-        }
+        public string Address { get; }
 
-        public string Name
-        {
-            get { return m_name; }
-        }
+        public string Name { get; }
 
         public override string ToString()
         {
-            return string.Format("{0}: {1}", Name, Address);
+            return $"{Name}: {Address}";
         }
     }
 }
