c3cb8d68b465d3eca99ee0f38554934385032826
Oren Novotny <oren@novotny.org>
2016-06-03 19:36:51 -0400

    Split into partial class

---
+++ Zeroconf.Shared/ZeroconfResolver.Observable.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Zeroconf
+{
+    static partial class ZeroconfResolver
+    {
+        /// <summary>
+        ///     Resolves available ZeroConf services
+        /// </summary>
+        /// <param name="scanTime">Default is 2 seconds</param>
+        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
+        /// <param name="protocol"></param>
+        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
+        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
+        /// <returns></returns>
+        public static IObservable<IZeroconfHost> Resolve(string protocol,
+                                                         TimeSpan scanTime = default(TimeSpan),
+                                                         int retries = 2,
+                                                         int retryDelayMilliseconds = 2000,
+                                                         bool bestInterface = false)
+        {
+            if (string.IsNullOrWhiteSpace(protocol))
+                throw new ArgumentNullException(nameof(protocol));
+
+            return Resolve(new[] { protocol }, scanTime, retries, retryDelayMilliseconds, bestInterface);
+        }
+
+
+
+        /// <summary>
+        ///     Resolves available ZeroConf services
+        /// </summary>
+        /// <param name="scanTime">Default is 2 seconds</param>
+        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
+        /// <param name="protocols"></param>
+        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
+        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
+        /// <returns></returns>
+        public static IObservable<IZeroconfHost> Resolve(IEnumerable<string> protocols,
+                                                         TimeSpan scanTime = default(TimeSpan),
+                                                         int retries = 2,
+                                                         int retryDelayMilliseconds = 2000,
+                                                         bool bestInterface = false)
+        {
+            if (protocols == null)
+                throw new ArgumentNullException(nameof(protocols));
+
+
+            return Observable.Create<IZeroconfHost>(
+                async (obs, cxl) =>
+                {
+                    try
+                    {
+                        Action<IZeroconfHost> cb = obs.OnNext;
+                        await ResolveAsync(protocols, scanTime, retries, retryDelayMilliseconds, cb, bestInterface, cxl);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Nothing to do here, eat it and mark completed
+                    }
+                    catch (Exception e)
+                    {
+                        obs.OnError(e);
+                    }
+                    finally
+                    {
+                        obs.OnCompleted();
+                    }
+
+                });
+        }
+
+        public static IObservable<DomainService> BrowseDomains(TimeSpan scanTime = default(TimeSpan),
+                                                                             int retries = 2,
+                                                                             int retryDelayMilliseconds = 2000)
+        {
+            return Observable.Create<DomainService>(
+                async (obs, cxl) =>
+                {
+                    try
+                    {
+                        Action<string, string> cb = (d, s) => obs.OnNext(new DomainService(d, s));
+                        await BrowseDomainsAsync(scanTime, retries, retryDelayMilliseconds, cb, cxl);
+                    }
+                    catch (OperationCanceledException)
+                    { }
+                    catch (Exception e)
+                    {
+                        obs.OnError(e);
+                    }
+                    finally
+                    {
+                        obs.OnCompleted();
+                    }
+                });
+        }
+
+
+        /// <summary>
+        /// Listens for mDNS Service Announcements
+        /// </summary>
+        /// <returns></returns>
+        public static IObservable<ServiceAnnouncement> ListenForAnnouncementsAsync()
+        {
+            return Observable.Create<ServiceAnnouncement>(
+                async (obs, cxl) =>
+                {
+                    try
+                    {
+                        await ListenForAnnouncementsAsync(obs.OnNext, cxl);
+                    }
+                    catch (OperationCanceledException)
+                    { }
+                    catch (Exception e)
+                    {
+                        obs.OnError(e);
+                    }
+                    finally
+                    {
+                        obs.OnCompleted();
+                    }
+                });
+        }
+    }
+}
--- Zeroconf.Shared/ZeroconfResolver.cs
+++ Zeroconf.Shared/ZeroconfResolver.cs
@@ -12,7 +12,7 @@ namespace Zeroconf
     /// <summary>
     ///     Looks for ZeroConf devices
     /// </summary>
-    public static class ZeroconfResolver
+    public static partial class ZeroconfResolver
     {
         static readonly AsyncLock ResolverLock = new AsyncLock();
 
@@ -27,254 +27,9 @@ namespace Zeroconf
 #endif
         }
 
-        /// <summary>
-        ///     Resolves available ZeroConf services
-        /// </summary>
-        /// <param name="scanTime">Default is 2 seconds</param>
-        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
-        /// <param name="protocol"></param>
-        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
-        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
-        /// <returns></returns>
-        public static IObservable<IZeroconfHost> Resolve(string protocol,
-                                                         TimeSpan scanTime = default(TimeSpan),
-                                                         int retries = 2,
-                                                         int retryDelayMilliseconds = 2000,
-                                                         bool bestInterface = false)
-        {
-            if (string.IsNullOrWhiteSpace(protocol))
-                throw new ArgumentNullException(nameof(protocol));
-
-            return Resolve(new[] { protocol }, scanTime, retries, retryDelayMilliseconds, bestInterface);
-        }
-
-        /// <summary>
-        ///     Resolves available ZeroConf services
-        /// </summary>
-        /// <param name="scanTime">Default is 2 seconds</param>
-        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
-        /// <param name="protocols"></param>
-        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
-        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
-        /// <returns></returns>
-        public static IObservable<IZeroconfHost> Resolve(IEnumerable<string> protocols,
-                                                         TimeSpan scanTime = default(TimeSpan),
-                                                         int retries = 2,
-                                                         int retryDelayMilliseconds = 2000,
-                                                         bool bestInterface = false)
-        {
-            if (protocols == null)
-                throw new ArgumentNullException(nameof(protocols));
 
 
-            return Observable.Create<IZeroconfHost>(
-                async (obs, cxl) =>
-                {
-                    try
-                    {
-                        Action<IZeroconfHost> cb = obs.OnNext;
-                        await ResolveAsync(protocols, scanTime, retries, retryDelayMilliseconds, cb, bestInterface, cxl);
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        // Nothing to do here, eat it and mark completed
-                    }
-                    catch (Exception e)
-                    {
-                        obs.OnError(e);
-                    }
-                    finally
-                    {
-                        obs.OnCompleted();
-                    }
 
-                });
-        }
-
-        /// <summary>
-        ///     Resolves available ZeroConf services
-        /// </summary>
-        /// <param name="scanTime">Default is 2 seconds</param>
-        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
-        /// <param name="cancellationToken"></param>
-        /// <param name="protocol"></param>
-        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
-        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
-        /// <param name="callback">Called per record returned as they come in.</param>
-        /// <returns></returns>
-        public static Task<IReadOnlyList<IZeroconfHost>> ResolveAsync(string protocol,
-                                                                      TimeSpan scanTime = default (TimeSpan),
-                                                                      int retries = 2,
-                                                                      int retryDelayMilliseconds = 2000,
-                                                                      Action<IZeroconfHost> callback = null,
-                                                                      bool bestInterface = false,
-                                                                      CancellationToken cancellationToken = default (CancellationToken))
-        {
-            if (string.IsNullOrWhiteSpace(protocol))
-                throw new ArgumentNullException(nameof(protocol));
-
-            return ResolveAsync(new[] {protocol},
-                                scanTime,
-                                retries,
-                                retryDelayMilliseconds, callback, bestInterface, cancellationToken);
-        }
-
-        /// <summary>
-        ///     Resolves available ZeroConf services
-        /// </summary>
-        /// <param name="scanTime">Default is 2 seconds</param>
-        /// <param name="bestInterface">Use only the best interface or all interfaces</param>
-        /// <param name="cancellationToken"></param>
-        /// <param name="protocols"></param>
-        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
-        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
-        /// <param name="callback">Called per record returned as they come in.</param>
-        /// <returns></returns>
-        public static async Task<IReadOnlyList<IZeroconfHost>> ResolveAsync(IEnumerable<string> protocols,
-                                                                            TimeSpan scanTime = default (TimeSpan),
-                                                                            int retries = 2,
-                                                                            int retryDelayMilliseconds = 2000,
-                                                                            Action<IZeroconfHost> callback = null,
-                                                                            bool bestInterface = false,
-                                                                            CancellationToken cancellationToken = default (CancellationToken))
-        {
-            Action<string, Response> wrappedAction = null;
-            if (callback != null)
-            {
-                wrappedAction = (address, resp) =>
-                                {
-                                    var zc = ResponseToZeroconf(resp, address);
-                                    callback(zc);
-                                };
-            }
-
-            var protos = protocols.ToList(); // prevent multiple enumeration
-            var buffer = GetRequestBytes(protos);
-            var dict = await ResolveInternal(protos,
-                                             buffer,
-                                             scanTime,
-                                             retries,
-                                             retryDelayMilliseconds,
-                                             wrappedAction,
-                                             cancellationToken)
-                                 .ConfigureAwait(false);
-
-            return dict.Select(pair => ResponseToZeroconf(pair.Value, pair.Key)).ToList();
-        }
-
-        public static IObservable<DomainService> BrowseDomains(TimeSpan scanTime = default(TimeSpan),
-                                                                             int retries = 2,
-                                                                             int retryDelayMilliseconds = 2000)
-        {
-            return Observable.Create<DomainService>(
-                async (obs, cxl) =>
-                      {
-                          try
-                          {
-                              Action<string, string> cb = (d, s) => obs.OnNext(new DomainService(d, s));
-                              await BrowseDomainsAsync(scanTime, retries, retryDelayMilliseconds, cb, cxl);
-                          }
-                          catch (OperationCanceledException)
-                          { }
-                          catch (Exception e)
-                          {
-                              obs.OnError(e);
-                          }
-                          finally
-                          {
-                              obs.OnCompleted();
-                          }
-                      });
-        }
-
-        /// <summary>
-        ///     Returns all available domains with services on them
-        /// </summary>
-        /// <param name="scanTime">Default is 2 seconds</param>
-        /// <param name="cancellationToken"></param>
-        /// <param name="retries">If the socket is busy, the number of times the resolver should retry</param>
-        /// <param name="retryDelayMilliseconds">The delay time between retries</param>
-        /// <param name="callback">Called per record returned as they come in.</param>
-        /// <returns></returns>
-        public static async Task<ILookup<string, string>> BrowseDomainsAsync(TimeSpan scanTime = default (TimeSpan),
-                                                                             int retries = 2,
-                                                                             int retryDelayMilliseconds = 2000,
-                                                                             Action<string, string> callback = null,
-                                                                             CancellationToken cancellationToken = default (CancellationToken))
-        {
-            const string protocol = "_services._dns-sd._udp.local.";
-
-            Action<string, Response> wrappedAction = null;
-            if (callback != null)
-            {
-                wrappedAction = (address, response) =>
-                                {
-                                    foreach (var service in BrowseResponseParser(response))
-                                    {
-                                        callback(service, address);
-                                    }
-                                };
-            }
-
-            var protocols = new[] {protocol};
-            var buffer = GetRequestBytes(protocols);
-            var dict = await ResolveInternal(protocols,
-                                             buffer,
-                                             scanTime,
-                                             retries,
-                                             retryDelayMilliseconds,
-                                             wrappedAction,
-                                             cancellationToken)
-                                 .ConfigureAwait(false);
-
-            var r = from kvp in dict
-                    from service in BrowseResponseParser(kvp.Value)
-                    select new {Service = service, Address = kvp.Key};
-
-            return r.ToLookup(k => k.Service, k => k.Address);
-        }
-
-        /// <summary>
-        /// Listens for mDNS Service Announcements
-        /// </summary>
-        /// <param name="callback"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public static Task ListenForAnnouncementsAsync(Action<ServiceAnnouncement> callback, CancellationToken cancellationToken)
-        {
-            return NetworkInterface.ListenForAnnouncementsAsync((adapter, address, buffer) =>
-            {
-                var response = new Response(buffer);
-                if (response.IsQueryResponse)
-                    callback(new ServiceAnnouncement(adapter, ResponseToZeroconf(response, address)));
-            }, cancellationToken);
-        }
-
-        /// <summary>
-        /// Listens for mDNS Service Announcements
-        /// </summary>
-        /// <returns></returns>
-        public static IObservable<ServiceAnnouncement> ListenForAnnouncementsAsync()
-        {
-            return Observable.Create<ServiceAnnouncement> (
-                async (obs, cxl) =>
-                      {
-                          try
-                          {
-                              await ListenForAnnouncementsAsync(obs.OnNext, cxl);
-                          }
-                          catch(OperationCanceledException)
-                          { }
-                          catch (Exception e)
-                          {
-                              obs.OnError(e);
-                          }
-                          finally
-                          {
-                              obs.OnCompleted();
-                          }
-                      });
-        }
 
         static IEnumerable<string> BrowseResponseParser(Response response)
         {
@@ -298,26 +53,24 @@ namespace Zeroconf
 
                 var dict = new Dictionary<string, Response>();
 
-                Action<string, byte[]> converter = (address, buffer) =>
-                                                   {
-                                                       var resp = new Response(buffer);
-                                                       Debug.WriteLine("IP: {0}, Bytes: {1}, IsResponse: {2}",
-                                                                       address,
-                                                                       buffer.Length,
-                                                                       resp.header.QR);
+                Action<string, byte[]> converter =
+                    (address, buffer) =>
+                    {
+                        var resp = new Response(buffer);
+                        Debug.WriteLine($"IP: {address}, Bytes: {buffer.Length}, IsResponse: {resp.header.QR}");
 
-                                                       if (resp.header.QR)
-                                                       {
-                                                           lock (dict)
-                                                           {
-                                                               dict[address] = resp;
-                                                           }
+                        if (resp.header.QR)
+                        {
+                            lock (dict)
+                            {
+                                dict[address] = resp;
+                            }
 
-                                                           callback?.Invoke(address, resp);
-                                                       }
-                                                   };
+                            callback?.Invoke(address, resp);
+                        }
+                    };
 
-                Debug.WriteLine("Looking for {0} with scantime {1}", string.Join(", ", protocols), scanTime);
+                Debug.WriteLine($"Looking for {string.Join(", ", protocols)} with scantime {scanTime}");
 
                 await NetworkInterface.NetworkRequestAsync(requestBytes,
                                                            scanTime,
