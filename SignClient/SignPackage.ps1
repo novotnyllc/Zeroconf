35e548d7422e3f72fe97806e8a94862931663e69
Oren Novotny <oren@novotny.org>
2016-09-11 20:50:01 -0400

    Add code signing

---
+++ SignClient/SignPackage.ps1
@@ -0,0 +1,25 @@
+$currentDirectory = split-path $MyInvocation.MyCommand.Definition
+
+# See if we have the ClientSecret available
+if([string]::IsNullOrEmpty($env:SignClientSecret)){
+	Write-Host "Client Secret not found, not signing packages"
+	return;
+}
+
+# Setup Variables we need to pass into the sign client tool
+
+$appSettings = "$currentDirectory\SignClient\appsettings.json"
+
+$appPath = "$currentDirectory\packages\tools\SignClient.dll"
+
+$nupgks = ls $currentDirector\*.nupkg | Select -ExpandProperty FullName
+
+foreach ($nupkg in $nupgks){
+	Write-Host "Submitting $nupkg for signing"
+
+	dotnet $appPath $appSettings $nupkg $nupkg 'zip' $env:SignClientSecret 'Zeroconf' 'https://github.com/onovotny/zeroconf' 
+
+	Write-Host "Finished signing $nupkg"
+}
+
+Write-Host "Sign-package complete"
