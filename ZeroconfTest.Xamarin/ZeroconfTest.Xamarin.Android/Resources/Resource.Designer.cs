db5db6253b9c890941ea0a94e3210f33fd2e1c59
Oren Novotny <oren@novotny.org>
2016-12-12 14:11:04 -0500

    update to latest rc

--- ZeroconfTest.Xamarin/ZeroconfTest.Xamarin.Android/Resources/Resource.Designer.cs
+++ ZeroconfTest.Xamarin/ZeroconfTest.Xamarin.Android/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace ZeroconfTest.Xam.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::ZeroconfTest.Xam.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
