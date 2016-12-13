039c56d6e03130c62a2b08d0c2a4384b5964bb60
Oren Novotny <oren@novotny.org>
2016-12-13 12:28:01 -0500

    single proj

--- Zeroconf.RT/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: NeutralResourcesLanguage("en-US")]
