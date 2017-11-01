ed87dbbf5d766ad7a048f9d9bd8c6c5ff413bef2
Matthew Little <matt@pingman.com>
2017-11-01 16:48:27 -0600

    Added AllowOverlappedQueries property to facilitate streaming results without waiting for the lock around domain resolves

--- Zeroconf/ZeroconfResolver.cs
+++ Zeroconf/ZeroconfResolver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,7 +29,7 @@ namespace Zeroconf
                                                                          CancellationToken cancellationToken)
         {
             var requestBytes = GetRequestBytes(options);
-            using (await ResolverLock.LockAsync())
+            using (options.AllowOverlappedQueries ? Disposable.Empty : await ResolverLock.LockAsync())
             {
                 cancellationToken.ThrowIfCancellationRequested();
                 var dict = new Dictionary<string, Response>();
