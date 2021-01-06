da756a39b2364460f06e2d317ea7d971a7b2aa38
Andy Lee <andy.lee@spartanbio.com>
2021-01-06 08:16:31 -0500

    Added .Net Framework Test Env

---
+++ ZeroconfTest.NetFramework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeroconf;
+
+namespace ZeroconfTest.NetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = ZeroconfResolver.ResolveAsync("_spartancube._tcp.local.");
+            task.Wait();
+            foreach (var l in task.Result)
+            {
+                Console.WriteLine(l.DisplayName);
+                Console.WriteLine(l.NetworkInterface.Description);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ZeroconfTest.NetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZeroconfTest.NetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZeroconfTest.NetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20448f47-ba5e-4348-8f31-f20f190dea96")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
